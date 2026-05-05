fbbc60f5b5151cbd13c035ae2ff32a45b20ba222
PlnThmrs <pauline.thomieres@gmail.com>
2026-05-05 14:33:19 +0200

    Ajout des tests, lint, format et analyse sécurité

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).resolve().parent
-SRC_PATH = PROJECT_ROOT / "src"
-sys.path.insert(0, str(SRC_PATH))
-
 from src.ml_housing.pipeline import run_pipeline
-
 
 if __name__ == "__main__":
     metrics = run_pipeline()
